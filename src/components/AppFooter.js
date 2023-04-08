4be7112a1e24618b73a55d1334486ffa151394ed
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2023-04-08 22:54:27 +0100

    update footer

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,16 +5,15 @@ const AppFooter = () => {
   return (
     <CFooter>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
+        <a href="https://binnox.app/" target="_blank" rel="noopener noreferrer">
+          Binnox
         </a>
-        <span className="ms-1">&copy; 2022 creativeLabs.</span>
+        <span className="ms-1">&copy; 2023 </span>
       </div>
       <div className="ms-auto">
-        <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI React Admin &amp; Dashboard Template
-        </a>
+        {/* <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer"> */}
+        Binnox Admin Dashboard
+        {/* </a> */}
       </div>
     </CFooter>
   )

839b5fee91954a8d9fb76307a36f073c8f105e3a
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2023-04-09 13:56:45 +0100

    rearrange orders page

--- src/views/orders/Orders.js
+++ src/views/orders/Orders.js
@@ -8,12 +8,10 @@ import {
   CCardHeader,
   CCol,
   CDropdown,
-  CDropdownDivider,
   CDropdownItem,
   CDropdownMenu,
   CDropdownToggle,
   CFormCheck,
-  CProgress,
   CRow,
   CTable,
   CTableBody,
@@ -23,159 +21,20 @@ import {
   CTableRow,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import {
-  cibCcAmex,
-  cibCcApplePay,
-  cibCcMastercard,
-  cibCcPaypal,
-  cibCcStripe,
-  cibCcVisa,
-  cifBr,
-  cifEs,
-  cifFr,
-  cifIn,
-  cifPl,
-  cifUs,
-  cilPeople,
-} from '@coreui/icons'
+import { cilPeople } from '@coreui/icons'
 
-import avatar1 from 'src/assets/images/avatars/1.jpg'
-import avatar2 from 'src/assets/images/avatars/2.jpg'
-import avatar3 from 'src/assets/images/avatars/3.jpg'
-import avatar4 from 'src/assets/images/avatars/4.jpg'
-import avatar5 from 'src/assets/images/avatars/5.jpg'
-import avatar6 from 'src/assets/images/avatars/6.jpg'
-import axios from 'axios'
-import { useCookies } from 'react-cookie'
+import { AdminContext } from 'src/context/adminContext'
 
 function Orders() {
-  const tableExample = [
-    {
-      avatar: { src: avatar1, status: 'success' },
-      user: {
-        name: 'Yiorgos Avraamu',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'USA', flag: cifUs },
-      usage: {
-        value: 50,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'success',
-      },
-      payment: { name: 'Mastercard', icon: cibCcMastercard },
-      activity: '10 sec ago',
-    },
-    {
-      avatar: { src: avatar2, status: 'danger' },
-      user: {
-        name: 'Avram Tarasios',
-        new: false,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Brazil', flag: cifBr },
-      usage: {
-        value: 22,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'info',
-      },
-      payment: { name: 'Visa', icon: cibCcVisa },
-      activity: '5 minutes ago',
-    },
-    {
-      avatar: { src: avatar3, status: 'warning' },
-      user: { name: 'Quintin Ed', new: true, registered: 'Jan 1, 2021' },
-      country: { name: 'India', flag: cifIn },
-      usage: {
-        value: 74,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'warning',
-      },
-      payment: { name: 'Stripe', icon: cibCcStripe },
-      activity: '1 hour ago',
-    },
-    {
-      avatar: { src: avatar4, status: 'secondary' },
-      user: { name: 'Enéas Kwadwo', new: true, registered: 'Jan 1, 2021' },
-      country: { name: 'France', flag: cifFr },
-      usage: {
-        value: 98,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'danger',
-      },
-      payment: { name: 'PayPal', icon: cibCcPaypal },
-      activity: 'Last month',
-    },
-    {
-      avatar: { src: avatar5, status: 'success' },
-      user: {
-        name: 'Agapetus Tadeáš',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Spain', flag: cifEs },
-      usage: {
-        value: 22,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'primary',
-      },
-      payment: { name: 'Google Wallet', icon: cibCcApplePay },
-      activity: 'Last week',
-    },
-    {
-      avatar: { src: avatar6, status: 'danger' },
-      user: {
-        name: 'Friderik Dávid',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Poland', flag: cifPl },
-      usage: {
-        value: 43,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'success',
-      },
-      payment: { name: 'Amex', icon: cibCcAmex },
-      activity: 'Last week',
-    },
-  ]
-  const [orderList, setOrderList] = React.useState({
-    loading: true,
-    data: [],
-  })
-  const [cookies] = useCookies()
-  async function getBusinessRecords() {
-    // let apiUrl = 'http://localhost:5000/api'
-    let apiUrl = 'https://binnox.herokuapp.com/api'
-    // url: `${apiUrl}/auth/admin/login`,
-    // axios POST request
+  const { orderList } = React.useContext(AdminContext)
 
-    console.log(cookies.BinnoxAdmin.token)
-    axios
-      .get(`${apiUrl}/admin/orders`, {
-        headers: {
-          Authorization: cookies?.BinnoxAdmin?.token,
-        },
-      })
-      .then((res) => {
-        console.log('orders', res.data)
-        setOrderList({ loading: false, data: res.data.orders })
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-  }
-
-  React.useEffect(() => {
-    getBusinessRecords()
-  }, [])
   return (
     <CRow>
       <CCol xs>
         <CCard className="mb-4">
           <CCardHeader>Orders</CCardHeader>
           <CCardBody>
-            <CButtonGroup role="group" aria-label="Basic checkbox toggle button group">
+            {/* <CButtonGroup role="group" aria-label="Basic checkbox toggle button group">
               <CFormCheck
                 type="radio"
                 button={{ color: 'primary', variant: 'outline' }}
@@ -201,7 +60,7 @@ function Orders() {
                 autoComplete="off"
                 label="Radio 3"
               />
-            </CButtonGroup>
+            </CButtonGroup> */}
             <CTable align="middle" className="mb-0 border" hover responsive>
               <CTableHead color="light">
                 <CTableRow>
@@ -218,53 +77,6 @@ function Orders() {
                 </CTableRow>
               </CTableHead>
               <CTableBody>
-                {/* {tableExample.map((item, index) => (
-                  <CTableRow v-for="item in tableItems" key={index}>
-                    <CTableDataCell className="text-center">
-                      <CAvatar size="md" src={item.avatar.src} status={item.avatar.status} />
-                    </CTableDataCell>
-                    <CTableDataCell>
-                      <div>{item.user.name}</div>
-                      <div className="small text-medium-emphasis">
-                        <span>{item.user.new ? 'New' : 'Recurring'}</span> | Registered:{' '}
-                        {item.user.registered}
-                      </div>
-                    </CTableDataCell>
-                    <CTableDataCell className="text-center">
-                      <CIcon size="xl" icon={item.country.flag} title={item.country.name} />
-                    </CTableDataCell>
-                    <CTableDataCell>
-                      <div className="clearfix">
-                        <div className="float-start">
-                          <strong>{item.usage.value}%</strong>
-                        </div>
-                        <div className="float-end">
-                          <small className="text-medium-emphasis">{item.usage.period}</small>
-                        </div>
-                      </div>
-                      <CProgress thin color={item.usage.color} value={item.usage.value} />
-                    </CTableDataCell>
-                    <CTableDataCell className="text-center">
-                      <CIcon size="xl" icon={item.payment.icon} />
-                    </CTableDataCell>
-                    <CTableDataCell>
-                      <div className="small text-medium-emphasis">Last login</div>
-                      <strong>{item.activity}</strong>
-                    </CTableDataCell>
-                    <CTableDataCell>
-                      <CDropdown variant="btn-group">
-                        <CDropdownToggle color="primary">Actions</CDropdownToggle>
-                        <CDropdownMenu>
-                          <CDropdownItem href="#">Action</CDropdownItem>
-                          <CDropdownItem href="#">Another action</CDropdownItem>
-                          <CDropdownItem href="#">Something else here</CDropdownItem>
-                          <CDropdownDivider />
-                          <CDropdownItem href="#">Separated link</CDropdownItem>
-                        </CDropdownMenu>
-                      </CDropdown>
-                    </CTableDataCell>
-                  </CTableRow>
-                ))} */}
                 {orderList.loading ? (
                   <>loading...</>
                 ) : (

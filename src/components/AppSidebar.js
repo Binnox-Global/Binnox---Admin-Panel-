7cf884b90f5c2a93654e08bfaff7355b5b5a6864
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2023-04-09 15:56:24 +0100

    update logos

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -2,12 +2,11 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
 
 import { AppSidebarNav } from './AppSidebarNav'
 
-import { logoNegative } from 'src/assets/brand/logo-negative'
-import { sygnet } from 'src/assets/brand/sygnet'
+import logo from '../assets/brand/binnox_logo.png'
+import logoSm from '../assets/brand/binnox_logo_small.png'
 
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
@@ -30,8 +29,8 @@ const AppSidebar = () => {
       }}
     >
       <CSidebarBrand className="d-none d-md-flex" to="/">
-        <CIcon className="sidebar-brand-full" icon={logoNegative} height={35} />
-        <CIcon className="sidebar-brand-narrow" icon={sygnet} height={35} />
+        <img className="sidebar-brand-full" src={logo} height={35} />
+        <img className="sidebar-brand-narrow" src={logoSm} height={35} />
       </CSidebarBrand>
       <CSidebarNav>
         <SimpleBar>

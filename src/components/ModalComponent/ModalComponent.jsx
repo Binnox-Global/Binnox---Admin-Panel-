df91c2d15798fa1d96ee7b959807af5f3d6f0d87
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2023-07-01 00:44:23 +0100

    created modal component

---
+++ src/components/ModalComponent/ModalComponent.jsx
@@ -0,0 +1,33 @@
+import { CButton, CModal, CModalBody, CModalHeader, CModalTitle } from '@coreui/react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { AdminContext } from 'src/context/adminContext'
+
+function ModalComponent({ component, title, btnText }) {
+  const { modalComponentVisible, setModalComponentVisible } = React.useContext(AdminContext)
+  return (
+    <>
+      <CButton className="btn-sm" onClick={() => setModalComponentVisible(true)}>
+        {btnText}
+      </CButton>
+      <CModal
+        fullscreen="xxl"
+        visible={modalComponentVisible}
+        onClose={() => setModalComponentVisible(false)}
+      >
+        <CModalHeader>
+          <CModalTitle>{title}</CModalTitle>
+        </CModalHeader>
+        <CModalBody>{component}</CModalBody>
+      </CModal>
+    </>
+  )
+}
+
+ModalComponent.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+  title: PropTypes.string,
+  btnText: PropTypes.string,
+}
+
+export default ModalComponent

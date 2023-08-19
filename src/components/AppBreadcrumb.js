9fea0ca812088b8cee781ddc2a5ec7bb87683f14
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2023-08-19 18:42:06 +0100

    reload button

--- src/components/AppBreadcrumb.js
+++ src/components/AppBreadcrumb.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useLocation } from 'react-router-dom'
 
 import routes from '../routes'
 
 import { CBreadcrumb, CBreadcrumbItem } from '@coreui/react'
+import Buttons from './../views/buttons/buttons/Buttons'
+import { AdminContext } from 'src/context/adminContext'
 
 const AppBreadcrumb = () => {
+  const { refreshDashboardFunction } = useContext(AdminContext)
   const currentLocation = useLocation().pathname
   // console.log('currentLocation', currentLocation)
 
@@ -37,26 +40,32 @@ const AppBreadcrumb = () => {
   const breadcrumbs = getBreadcrumbs(currentLocation)
 
   return (
-    <CBreadcrumb className="m-0 ms-2">
-      {/* <CBreadcrumbItem href="/dashboard">Dashboard</CBreadcrumbItem> */}
-      {breadcrumbs.map((breadcrumb, index) => {
-        return (
-          <CBreadcrumbItem
-            className="text-capitalize"
-            key={index}
-            // {...(breadcrumb.active ? { active: true } : { href: breadcrumb.pathname })}
-          >
-            {breadcrumb}
-          </CBreadcrumbItem>
-          // <CBreadcrumbItem
-          //   {...(breadcrumb.active ? { active: true } : { href: breadcrumb.pathname })}
-          //   key={index}
-          // >
-          //   {breadcrumb.name}
-          // </CBreadcrumbItem>
-        )
-      })}
-    </CBreadcrumb>
+    <>
+      {' '}
+      <CBreadcrumb className="m-0 ms-2">
+        {/* <CBreadcrumbItem href="/dashboard">Dashboard</CBreadcrumbItem> */}
+        {breadcrumbs.map((breadcrumb, index) => {
+          return (
+            <CBreadcrumbItem
+              className="text-capitalize"
+              key={index}
+              // {...(breadcrumb.active ? { active: true } : { href: breadcrumb.pathname })}
+            >
+              {breadcrumb}
+            </CBreadcrumbItem>
+            // <CBreadcrumbItem
+            //   {...(breadcrumb.active ? { active: true } : { href: breadcrumb.pathname })}
+            //   key={index}
+            // >
+            //   {breadcrumb.name}
+            // </CBreadcrumbItem>
+          )
+        })}
+      </CBreadcrumb>
+      <button className=" btn btn-primary" onClick={() => refreshDashboardFunction()}>
+        ReLoad
+      </button>
+    </>
   )
 }
 

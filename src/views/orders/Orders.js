885db6bc9c9ba911062ed2fec8e9f489b5e70128
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2023-08-29 17:11:15 +0100

    clear comment

--- src/views/orders/Orders.js
+++ src/views/orders/Orders.js
@@ -456,33 +456,6 @@ export function OrdersPickedUp() {
         <CCard className="mb-4">
           <CCardHeader>Orders Picked up</CCardHeader>
           <CCardBody>
-            {/* <CButtonGroup role="group" aria-label="Basic checkbox toggle button group">
-              <CFormCheck
-                type="radio"
-                button={{ color: 'primary', variant: 'outline' }}
-                name="btnradio"
-                id="btnradio1"
-                autoComplete="off"
-                label="Radio 1"
-                defaultChecked
-              />
-              <CFormCheck
-                type="radio"
-                button={{ color: 'primary', variant: 'outline' }}
-                name="btnradio"
-                id="btnradio2"
-                autoComplete="off"
-                label="Radio 2"
-              />
-              <CFormCheck
-                type="radio"
-                button={{ color: 'primary', variant: 'outline' }}
-                name="btnradio"
-                id="btnradio3"
-                autoComplete="off"
-                label="Radio 3"
-              />
-            </CButtonGroup> */}
             <CTable
               align="middle"
               className="mb-0 border"

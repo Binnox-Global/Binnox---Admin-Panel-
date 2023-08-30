14a30ff7bc1ba0a59a5a95db8ecd8905ac62f92b
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2023-08-30 10:24:24 +0100

    remove Picked up from nav

--- src/_nav.js
+++ src/_nav.js
@@ -88,19 +88,19 @@ const _nav = [
       },
       {
         component: CNavItem,
-        name: 'Order Transfer',
-        to: '/orders/transfer',
-      },
-      {
-        component: CNavItem,
         name: 'New Orders',
         to: '/orders/new',
       },
       {
         component: CNavItem,
-        name: 'Picked up',
-        to: '/orders/picked-up',
+        name: 'Order Transfer',
+        to: '/orders/transfer',
       },
+      // {
+      //   component: CNavItem,
+      //   name: 'Picked up',
+      //   to: '/orders/picked-up',
+      // },
       {
         component: CNavItem,
         name: 'Delivered',

b2fefa5f04106dfce1c17ed9b491382f51f7dfc3
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2023-09-21 18:28:01 +0100

    show chat

--- src/components/Chart/ChartTest.jsx
+++ src/components/Chart/ChartTest.jsx
@@ -15,6 +15,7 @@ import 'chartjs-adapter-moment' // Import the 'chartjs-adapter-moment'
 import { faker } from '@faker-js/faker'
 import { AdminContext } from 'src/context/adminContext'
 import moment from 'moment'
+import DateRangePicker from '../DateRange/DateRangePicker'
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 
@@ -69,11 +70,47 @@ export const options = {
 // }
 
 export function ChartTest() {
+  const [showData, setShowData] = useState([])
+  const [startDate, setStartDate] = useState(null)
+  const [endDate, setEndDate] = useState(null)
+  const [filteredData, setFilteredData] = useState(null)
   const [data, setData] = useState({
     labels: [],
     datasets: [],
   })
   const { groupedOrderData, generalOrder } = useContext(AdminContext)
+  // Function to filter data by date range
+  function filterDataByDateRange(data, startDate, endDate) {
+    // Convert start and end dates to Date objects
+
+    const startDateObj = new Date(startDate)
+    const endDateObj = new Date(endDate)
+    console.log('data', data)
+    // Use the filter method to select objects within the date range
+    const filteredData = data?.filter((item) => {
+      // Convert start and end dates format
+      let itemDateArray = item?.date.split('/')
+      let newItemDate = `${itemDateArray[1]} / ${itemDateArray[0]}/ ${itemDateArray[2]}
+      `
+      // Convert the item's date to a Date object
+      const itemDate = new Date(newItemDate)
+      console.log('itemDate', item?.date)
+      console.log('newItemDate', newItemDate)
+      console.log('itemDate', itemDate)
+      // Check if the item's date is within the selected range
+      return itemDate >= startDateObj && itemDate <= endDateObj
+    })
+    // console.log('filteredData', filteredData)
+    // return filteredData.reverse()
+    return filteredData
+  }
+
+  function clearFilter() {
+    setStartDate(null)
+    setEndDate(null)
+    setFilteredData(null)
+    // setShowData(groupedOrderData.data)
+  }
 
   useEffect(() => {
     // console.log('groupedOrderData', groupedOrderData)
@@ -88,8 +125,9 @@ export function ChartTest() {
     let dataSetsArrayDataOrderCount = []
     let dataSetsArrayDataTotalAmount = []
     let labelsArrayData = []
-
-    groupedOrderData?.data?.reverse()?.forEach((item) => {
+    // console.log('showData', showData)
+    // showData?.forEach((item) => {
+    showData?.forEach((item) => {
       // console.log(item)
       let rearrangeDateArray = item?.date.split('/')
       let rearrangeDate = `${rearrangeDateArray[1]}/${rearrangeDateArray[0]}/${rearrangeDateArray[2]}`
@@ -123,12 +161,62 @@ export function ChartTest() {
       labels: labelsArray,
       datasets: dataSetsArray,
     })
-  }, [groupedOrderData])
+  }, [showData])
+
   useEffect(() => {
+    if (!filteredData) {
+      // setShowData(groupedOrderData.data.reverse())
+      setShowData(groupedOrderData.data.reverse())
+    } else {
+      setShowData(filteredData.data.reverse())
+    }
+  }, [startDate, endDate, filteredData, groupedOrderData])
+  // useEffect(() => {
+  //   console.log('showData', showData)
+  // }, [showData])
+
+  useEffect(() => {
+    // console.log('groupedOrderData', groupedOrderData)
     // console.log('data', data)
-  }, [data])
+    if (!startDate || !endDate) return
+    const filteredData = filterDataByDateRange(groupedOrderData.data, startDate, endDate)
+    // console.log('filteredData', filteredData)
+    setFilteredData({ data: filteredData })
+  }, [startDate, endDate, groupedOrderData])
+
+  const handleDateChange = (type, date) => {
+    if (type === 'start') {
+      setStartDate(date)
+    } else if (type === 'end') {
+      setEndDate(date)
+    }
+  }
   return (
     <>
+      <div className="">
+        {/* <h1>Date Range Picker Example</h1> */}
+        <div className="d-flex flex-wrap">
+          {' '}
+          <DateRangePicker
+            startDate={startDate}
+            endDate={endDate}
+            onChange={handleDateChange}
+          />{' '}
+          <button
+            onClick={() => {
+              clearFilter()
+            }}
+          >
+            Clear Filter
+          </button>
+        </div>
+        <p>
+          Selected Date Range:{' '}
+          {startDate && endDate
+            ? `${startDate.toDateString()} - ${endDate.toDateString()}`
+            : 'Select a date range'}
+        </p>
+      </div>
       <Line options={options} data={data} />
       {/* {data.datasets.forEach((count, i) => {
         return (

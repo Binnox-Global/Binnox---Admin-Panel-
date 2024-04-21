117a1062e92fb6e94d66bf9c95e0c5ab6c0075ef
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2024-04-21 16:11:09 +0100

    correct the flow

--- src/context/socketContext.jsx
+++ src/context/socketContext.jsx
@@ -12,7 +12,8 @@ import io from 'socket.io-client'
 export const SocketContext = createContext()
 
 function SocketProvider({ children }) {
-  const { token, setOrderList, orderList, setOrderGroupTransferList } = useContext(AdminContext)
+  const { token, setOrderList, setOrderGroupList, orderList, setOrderGroupTransferList } =
+    useContext(AdminContext)
   const [testOrder, setTestOrder] = useState({
     loading: true,
     data: [],
@@ -20,26 +21,22 @@ function SocketProvider({ children }) {
   const [socket, setSocket] = useState(null)
   const [cookies, removeCookie] = useCookies()
 
+  // const socketUrl = 'https://binnox-socket-c7299d8dfb25.herokuapp.com'
+  const socketUrl = 'http://localhost:1000'
+
   useEffect(
     function () {
       if (!token) return
-      // const socket = io('http://localhost:1000', {
-      //   query: {
-      //     token: token, // Include your JWT token here
-      //   },
-      // }) // Replace with your server URL
-      // Connect to the Socket.IO server
-      const socket = io('https://binnox-socket-c7299d8dfb25.herokuapp.com', {
+      const socket = io(socketUrl, {
         query: {
-          token: cookies?.BinnoxAdmin.token, // Include your JWT token here
+          token: token, // Include your JWT token here
         },
       }) // Replace with your server URL
 
-      setSocket(socket)
-      //
       // Event handler for 'connect' event
       socket.on('connect', () => {
         console.log('Connected to server')
+        setSocket((prev) => socket)
       })
 
       // Event handler for 'disconnect' event
@@ -56,55 +53,10 @@ function SocketProvider({ children }) {
     [token],
   )
 
-  useEffect(
-    function () {
-      // Connect to the Socket.IO server
-      if (!socket) return
-
-      // Event handler for 'message' event
-      socket.on('admin_getOrder', (data) => {
-        console.log('Received data:', data)
-        // setTestOrder({ loading: false, data })
-        setOrderList({ loading: false, data: data.reverse() })
-      })
-
-      // socket.on('admin_orderWithTransfer', (data) => {
-      //   setOrderGroupTransferList({
-      //     loading: false,
-      //     data: {
-      //       new: [],
-      //       accepted: [],
-      //       rejected: [],
-      //     },
-      //   })
-      //   setOrderGroupTransferList((prevOrderGroupTransferList) => ({
-      //     ...prevOrderGroupTransferList,
-      //     loading: false,
-      //     data: {
-      //       new: data.pendingTransfer.reverse(),
-      //       accepted: data.approvedTransfer.reverse(),
-      //       rejected: data.rejectedTransfer.reverse(),
-      //     },
-      //   }))
-      //   console.log('admin_orderWithTransfer', data)
-      // })
-
-      // Event handler for 'message' event
-      socket.on('Error', (data) => {
-        // setError(data)
-        console.log('Error', data)
-      })
-
-      adminGetOrderTransfersSocketFunction()
-    },
-    [socket],
-  )
-
   useEffect(() => {
     if (!socket) return
     socket.on('admin_orderWithTransfer', (data) => {
-      console.log('admin_orderWithTransfer', data)
-      adminGetOrderTransfersSocketFunction()
+      // console.log('admin_orderWithTransfer', data)
       setOrderGroupTransferList((prevOrderGroupTransferList) => ({
         ...prevOrderGroupTransferList,
         loading: false,
@@ -115,47 +67,27 @@ function SocketProvider({ children }) {
         },
       }))
     })
+    // socket to get orders
+    socket.on('admin_orders', (data) => {
+      // console.log('admin_orders', data)
+      setOrderGroupList((prevOrderGroupList) => ({
+        ...prevOrderGroupList,
+        loading: false,
+        data: data,
+      }))
+    })
 
     // Clean up the event listener
     return () => {
       socket.off('admin_orderWithTransfer')
+      socket.off('admin_orders')
     }
   }, [socket]) // Empty dependency array ensures that this effect runs only once
 
-  function adminGetOrderTransfersSocketFunction() {
-    console.log('adminGetOrderTransfersSocketFunction called')
-    // Event handler for 'message' event
-    socket.emit('admin_getOrderWithTransfer', {}, (callback) => {
-      console.log('Received data:', { data: callback })
-      // setOrderList({ loading: false, data: callback.data?.reverse() })
-      setOrderGroupTransferList((prevOrderGroupTransferList) => ({
-        ...prevOrderGroupTransferList,
-        loading: false,
-        data: {
-          new: callback.pendingTransfer.reverse(),
-          accepted: callback.approvedTransfer.reverse(),
-          rejected: callback.rejectedTransfer.reverse(),
-        },
-      }))
-    })
-  }
-
-  function adminGetOrdersSocketFunction() {
-    // console.log('adminGetOrdersSocketFunction called')
-
-    if (!socket) return
-    // Event handler for 'message' event
-    socket.emit('admin_getPendingOrders', {}, (callback) => {
-      // console.log('Received data:', { data: callback.data })
-      setOrderList({ loading: false, data: callback.data?.reverse() })
-    })
-  }
   return (
     <SocketContext.Provider
       value={{
         socket,
-        adminGetOrdersSocketFunction,
-        adminGetOrderTransfersSocketFunction,
       }}
     >
       {children}

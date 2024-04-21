3f6359d823e766aef5fa8bfcef350e4131dab769
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2024-04-21 18:41:17 +0100

    try to correct sound and notification

--- src/context/socketContext.jsx
+++ src/context/socketContext.jsx
@@ -29,31 +29,65 @@ function SocketProvider({ children }) {
   // }, [])
   const [isNotifying, setIsNotifying] = useState(false)
 
-  const triggerNotification = (order) => {
+  const triggerNotification = ({ notificationHeader, notificationMessage }) => {
+    console.log('triggerNotification called')
     if (isNotifying) return // Prevent duplicate notifications
     setIsNotifying(true)
 
     // Customize the notification sound if supported by the browser
     const audio = new Audio('/Binnox.webm') // Replace with your sound file
-    // console.log({ audio })
-    audio.addEventListener('error', (e) => {
-      console.error('Error loading audio:', e)
-    })
+    // // console.log({ audio })
+    // audio.addEventListener('error', (e) => {
+    //   console.error('Error loading audio:', e)
+    // })
 
     // audio.muted = true // Mute the audio initially
     audio.play().catch((error) => {
       console.error('Error playing audio:', error)
     })
 
-    // Use the browser's Notification API to show a notification
-    if ('Notification' in window) {
-      const notification = new Notification('New Order Received', {
-        body: `Order ID: 123456993`,
+    setIsNotifying(false)
+
+    if (permission === 'granted') {
+      const notification = new Notification(notificationHeader || 'Test Notification', {
+        body: notificationMessage || 'This is a Test Notification From Binnox',
+        icon: '/icon.png',
+        requireInteraction: true, // Add this option
       })
-      notification.onclose = () => {
-        setIsNotifying(false) // Reset the flag when the notification is closed
+
+      notification.onshow = () => {
+        console.log('Notification shown')
       }
+
+      notification.onclick = () => {
+        console.log('Notification clicked')
+        // audio.muted = true // Mute the audio initially
+        // Later, to stop the sound...
+        audio.pause() // or audio.stop()
+        // Handle click event
+      }
+
+      notification.onclose = () => {
+        // audio.muted = true // Mute the audio initially
+        // Later, to stop the sound...
+        audio.pause() // or audio.stop()
+        console.log('Notification closed')
+      }
+
+      setNotification(notification)
+    } else {
+      console.log('Permission denied')
     }
+
+    // // Use the browser's Notification API to show a notification
+    // if ('Notification' in window) {
+    //   const notification = new Notification('New Order Received', {
+    //     body: `Order ID: 123456993`,
+    //   })
+    //   notification.onclose = () => {
+    //     setIsNotifying(false) // Reset the flag when the notification is closed
+    //   }
+    // }
   }
 
   useEffect(
@@ -84,12 +118,30 @@ function SocketProvider({ children }) {
     },
     [token],
   )
+  const [permission, setPermission] = useState(null)
+  const [notification, setNotification] = useState(null)
+
+  useEffect(() => {
+    requestPermission()
+  }, [])
+
+  const requestPermission = () => {
+    Notification.requestPermission().then((permission) => {
+      setPermission(permission)
+    })
+  }
 
   useEffect(() => {
     if (!socket) return
     socket.on('admin_orderWithTransfer', (data) => {
       // console.log('admin_orderWithTransfer', data)
-      triggerNotification()
+
+      if (data.pendingTransfer?.length) {
+        triggerNotification({
+          notificationMessage: `They are (${data.pendingTransfer?.length}) unattended order with transfer`,
+          notificationHeader: data.pendingTransfer?.length + ' Order With Transfer',
+        })
+      }
       setOrderGroupTransferList((prevOrderGroupTransferList) => ({
         ...prevOrderGroupTransferList,
         loading: false,
@@ -102,7 +154,21 @@ function SocketProvider({ children }) {
     })
     // socket to get orders
     socket.on('admin_orders', (data) => {
-      triggerNotification()
+      // triggerNotification()
+      let pendingOrders = []
+      data.forEach((order) => {
+        // console.log(order.statues)
+        if (order.statues === 'Pending') {
+          pendingOrders.push(order)
+        }
+      })
+
+      if (pendingOrders?.length) {
+        triggerNotification({
+          notificationMessage: `They are (${pendingOrders?.length}) unattended order`,
+          notificationHeader: pendingOrders?.length + ' Order Notification',
+        })
+      }
       // console.log('admin_orders', data)
       setOrderGroupList((prevOrderGroupList) => ({
         ...prevOrderGroupList,
